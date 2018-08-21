4eeef7c0cc51bbbbcef6792502956e626e709901
danbates2 <dgbates2@gmail.com>
2018-08-21 13:04:42 +0200

    commit

--- pulses_test.py
+++ pulses_test.py
@@ -3,13 +3,14 @@
 import time
 import RPi.GPIO as GPIO
 
+PulseCount = 0
+pulse_pin1 = 21
+
 def pulse_add(pulse_pin1):
     print('Pulse rising!')
-    int PulseCount =+ 1
+    PulseCount =+ 1
     print "total pulses:" + str(PulseCount)
     
-GPIO.add_event_detect(pulse_pin1, GPIO.RISING, callback=pulse_add)
-
 def main():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pulse_pin1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -19,6 +20,7 @@ try:
     while True:
         main()
 except KeyboardInterrupt:
+    GPIO.cleanup()
     pass
 
 if __name__ == "__main__":
