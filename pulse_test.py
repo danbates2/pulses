95662e89c7157b8cb8be8cd17d336003ca1047f1
danbates2 <dgbates2@gmail.com>
2018-08-21 12:46:45 +0200

    commit

--- pulse_test.py
+++ pulse_test.py
@@ -9,3 +9,17 @@ def pulse_add(pulse_pin1):
     print "total pulses:" + str(PulseCount)
     
 GPIO.add_event_detect(pulse_pin1, GPIO.RISING, callback=pulse_add)
+
+def main():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pulse_pin1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(pulse_pin1, GPIO.RISING, callback=pulse_add, bouncetime=1)
+
+try:
+    while True:
+        main()
+except KeyboardInterrupt:
+    pass
+
+if __name__ == "__main__":
+        main()
